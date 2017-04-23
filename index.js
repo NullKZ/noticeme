e94d6aa456c0d5e09d383a94c14df44a638c07bb
Kevin Zou <kevinzou2150@gmail.com>
2017-04-22 23:11:26 -0400

    convert tab to 4space

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = function noticeMe(dispatch){
-	dispatch.hook('C_WHISPER', 1, (event) => {
-		if (event.target === 'n') {
-			dispatch.toServer('C_CHAT', 1, {
-				channel: 21,
-				message: event.message
-			})
-			return false
-		}
-	});
+     dispatch.hook('C_WHISPER', 1, (event) => {
+          if (event.target === 'n') {
+               dispatch.toServer('C_CHAT', 1, {
+                    channel: 21,
+                    message: event.message
+               })
+               return false
+          }
+     });
 }
